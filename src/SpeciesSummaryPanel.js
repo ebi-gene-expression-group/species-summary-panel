a617054781cf4f46a305e4cd13a7802590658871
Lingyun Zhao <33519183+lingyun1010@users.noreply.github.com>
2019-06-05 15:33:54 +0100

    Feature #164699431 add carousel species summary (#1)
    * Install local atlas-homepage-card package
    
    * Import CarousleCardsRow to replace ResponsiveCards
    
    * Fix a bug on importing withFetchLoader
    
    * Add stylesheet and props for carousle & classname
    
    * Mock matchMedia in test and update snapshots
    
    * Setup matchMedia in jest
    
    * Install the latest atlas-homepage-cards package
    
    * Correct carousel typos and remove slide css style
    
    * Update snapshots
    
    * Correct typo
    
    * Update atlas-homepage-cards package
    
    * Set adaptiveHeight as true! and add slide examples
    
    * Update snapshots
    
    * 1.0.0-rc
    
    * Update atlas cards package and pass slider props
    
    * Correct test with props and update snapshots
    
    * Restore the package version

--- src/SpeciesSummaryPanel.js
+++ src/SpeciesSummaryPanel.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import ResponsiveCardsRow from 'atlas-homepage-cards'
+import { CarouselCardsRow } from 'atlas-homepage-cards'
 
 const tabsId = `species-summary-tabs`
 
 class SpeciesSummaryPanel extends React.Component {
   render() {
-    const { speciesSummary, responsiveCardsRowProps } = this.props
+    const { speciesSummary, carouselCardsRowProps } = this.props
 
     return(
       [
@@ -24,9 +24,9 @@ class SpeciesSummaryPanel extends React.Component {
           {
             speciesSummary.map(({kingdom, cards}, idx) =>
               <div key={idx} className={`tabs-panel${idx === 0 ? ` is-active` : ``}`} id={kingdom}>
-                <ResponsiveCardsRow
+                <CarouselCardsRow
                   cards={cards}
-                  {...responsiveCardsRowProps}
+                  {...carouselCardsRowProps}
                 />
               </div>
             )
@@ -46,10 +46,10 @@ class SpeciesSummaryPanel extends React.Component {
 SpeciesSummaryPanel.propTypes = {
   speciesSummary: PropTypes.arrayOf(PropTypes.shape({
     kingdom: PropTypes.string.isRequired,
-    cards: ResponsiveCardsRow.propTypes.cards
+    cards: CarouselCardsRow.propTypes.cards
   })).isRequired,
   onComponentDidMount: PropTypes.func,
-  responsiveCardsRowProps: PropTypes.object
+  carouselCardsRowProps: PropTypes.object
 }
 
 SpeciesSummaryPanel.defaultProps = {
