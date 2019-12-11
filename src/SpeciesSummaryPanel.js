e389f604a4588867f39cf6b2129b2ef8d677af4c
amunozpomer <amunoz@ebi.ac.uk>
2019-12-11 15:13:34 +0000

    Org-scope dependencies

--- src/SpeciesSummaryPanel.js
+++ src/SpeciesSummaryPanel.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { CarouselCardsRow } from 'atlas-homepage-cards'
+import { CarouselCardsRow } from '@ebi-gene-expression-group/atlas-homepage-cards'
 
 const tabsId = `species-summary-tabs`
 
